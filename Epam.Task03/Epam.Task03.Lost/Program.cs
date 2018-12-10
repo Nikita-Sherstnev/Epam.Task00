2fbe705126e62d1c1eff11b60696ee7d690d50ff
Nikita <45091811+Nikita-Sherstnev@users.noreply.github.com>
2018-12-10 23:04:29 +0400

    Task03

---
+++ Epam.Task03/Epam.Task03.Lost/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Epam.Task03.Lost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = 0;
+            do
+            {
+                Console.WriteLine("Please, enter the number of people:");
+                num = int.TryParse(Console.ReadLine(), out num)
+                    ? num
+                    : 0;
+
+                if (num < 1)
+                {
+                    Console.WriteLine("Number must be natural!");
+                }
+            }
+            while (num < 1);
+
+            var list = new List<int>();
+
+            for (int i = 1; i <= num; i++)
+            {
+                list.Add(i);
+            }
+
+            bool delete = false;
+
+            while (list.Count > 1)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (delete)
+                    {
+                        list.RemoveAt(i--);
+                    }
+
+                    delete = !delete;
+                }
+
+                foreach (var item in list)
+                {
+                    Console.Write($"{item} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
