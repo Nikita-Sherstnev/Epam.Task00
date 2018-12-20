6388d70b55b66c78aa751c7b0eb7e9a0106c22ce
Nikita <45091811+Nikita-Sherstnev@users.noreply.github.com>
2018-12-20 17:54:12 +0400

    Task 3.2 added

---
+++ Epam.Task03/Epam.Task03.2.WordFrequency/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Epam.Task03._2.WordFrequency
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var wordFrequency = new WordFrequency();
+
+            wordFrequency.GetText();
+            wordFrequency.ClearingElements();
+            wordFrequency.ShowList();
+            wordFrequency.CountWords();
+        }
+    }
+}
---
+++ Epam.Task03/Epam.Task03.2.WordFrequency/WordFrequency.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Epam.Task03._2.WordFrequency
+{
+    public class WordFrequency
+    {
+        private char[] delimiterChars = { ' ', '.' };
+        private List<string> splitedText = new List<string>();
+        private int count = 1;
+        private int i;
+
+        public void GetText()
+        {
+            Console.WriteLine("Please, enter your english text:");
+            splitedText = Console.ReadLine().Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+
+        public void ShowList()
+        {
+            Console.WriteLine("List of words:");
+            for (int i = 0; i < splitedText.Count; i++)
+            {
+                Console.WriteLine(splitedText[i]);
+            }
+
+            Console.WriteLine();
+        }
+
+        public List<string> ClearingElements()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < splitedText.Count; i++)
+            {
+                var word = splitedText[i].ToLower();
+
+                for (int a = 0; a < word.Length; a++)
+                {
+                    var c = word[a];
+                    if (c >= 'a' && c <= 'z')
+                    {
+                        sb.Append(c);
+                    }
+                }
+
+                splitedText[splitedText.IndexOf(splitedText[i])] = sb.ToString();
+                sb.Clear();
+            }
+
+            return splitedText;
+        }
+
+        public void CountWords()
+        {
+            for (i = 1; splitedText.Count > 0; i++)
+            {
+                if (splitedText[0] == splitedText[i])
+                {
+                    count++;
+                    splitedText.RemoveAt(i);
+                    i--;
+                }
+
+                if (i >= (splitedText.Count - 1))
+                {
+                    Console.WriteLine($"Word \"{splitedText[0]}\" appeared in the text {count} time/times.");
+                    splitedText.RemoveAt(0);
+                    i = 0;
+                    count = 1;
+                }
+            }
+        }
+    }
+}
