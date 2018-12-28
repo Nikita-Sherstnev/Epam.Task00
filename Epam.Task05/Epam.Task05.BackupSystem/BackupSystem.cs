bfea5edb1e7dc1473ecd47194ad8be135afc529e
Nikita <45091811+Nikita-Sherstnev@users.noreply.github.com>
2018-12-28 14:43:29 +0400

    Add files via upload

--- Epam.Task05/Epam.Task05.BackupSystem/BackupSystem.cs
+++ Epam.Task05/Epam.Task05.BackupSystem/BackupSystem.cs
@@ -1,170 +1,167 @@
 ﻿using System;
 using System.IO;
 
-namespace Epam.Task06.BackUpSystem
+namespace Epam.Task05.BackupSystem
 {
-    public class BackUpSystem
+    public class BackupSystem
     {
         private readonly FileSystemWatcher fileWatcher;
-        private StreamWriter logger = null;
-
-        public event EventHandler OnDataRestored;
-
         private readonly string trackingDirectory;
         private readonly string backupDirectory;
         private readonly string loggerFullpath;
         private readonly string filter;
+        private StreamWriter logger = null;
         private int filesCount = 0;
 
-        public BackUpSystem(string trackingDirectory, string backupDirectory, string filter, bool isTracking)
+        public BackupSystem(string trackingDirectory, string backupDirectory, string filter, bool isTracking)
         {
             this.trackingDirectory = trackingDirectory;
             this.backupDirectory = backupDirectory;
             this.filter = filter;
             loggerFullpath = Path.Combine(backupDirectory, "logger.txt");
+
             if (!Directory.Exists(backupDirectory) || !File.Exists(loggerFullpath))
             {
                 Directory.CreateDirectory(backupDirectory);
-                string[] files_in_backup = Directory.GetFiles(backupDirectory);
-                foreach (var file in files_in_backup)
+                string[] filesInBackup = Directory.GetFiles(backupDirectory);
+                foreach (var file in filesInBackup)
                 {
                     File.Delete(file);
                 }
 
                 File.Create(loggerFullpath).Close();
 
-                string[] all_tracking_files = Directory.GetFiles(trackingDirectory, "*" + filter, SearchOption.AllDirectories);
-                Save("Created", all_tracking_files);
+                string[] allTrackingFiles = Directory.GetFiles(trackingDirectory, "*" + filter, SearchOption.AllDirectories);
+                Save("Created", allTrackingFiles);
             }
             else
             {
-                var c = Directory.GetFiles(this.backupDirectory, "BackUp*" + filter, SearchOption.AllDirectories);
-                filesCount = c.Length;
+                var files = Directory.GetFiles(this.backupDirectory, "Backup*" + filter, SearchOption.AllDirectories);
+                filesCount = files.Length;
             }
 
             fileWatcher = new FileSystemWatcher(trackingDirectory);
             fileWatcher.IncludeSubdirectories = true;
-            fileWatcher.Changed += FileSystemWatcher_OnChanged;
-            fileWatcher.Created += FileSystemWatcher_OnCreated;
-            fileWatcher.Deleted += FileSystemWatcher_OnCreated;
-            fileWatcher.Renamed += FileSystemWatcher_OnRenamed;
-            fileWatcher.Error += FileSystemWatcher_OnError;
+            fileWatcher.Changed += FileSystemWatcherOnChanged;
+            fileWatcher.Created += FileSystemWatcherOnCreated;
+            fileWatcher.Deleted += FileSystemWatcherOnCreated;
+            fileWatcher.Renamed += FileSystemWatcherOnRenamed;
+            fileWatcher.Error += FileSystemWatcherOnError;
             fileWatcher.EnableRaisingEvents = isTracking;
         }
 
-        public void RestoreTo(DateTime restoration_time)
+        void Recovery(StreamReader reader, string action)
+        {
+            string restoringFile;
+            string renamedFile;
+            string backupFile;
+            string deletedFile;
+
+            switch (action)
+            {
+                case "Created":
+                    {
+                        restoringFile = reader.ReadLine();
+                        backupFile = reader.ReadLine();
+                        Directory.CreateDirectory(Path.GetDirectoryName(restoringFile));
+                        File.Copy(backupFile, restoringFile, true);
+                        break;
+                    }
+
+                case "Changed":
+                    {
+                        restoringFile = reader.ReadLine();
+                        backupFile = reader.ReadLine();
+                        Directory.CreateDirectory(Path.GetDirectoryName(restoringFile));
+                        File.Copy(backupFile, restoringFile, true);
+                        break;
+                    }
+
+                case "Deleted":
+                    {
+                        deletedFile = reader.ReadLine();
+                        File.Delete(reader.ReadLine());
+                        break;
+                    }
+
+                case "Renamed":
+                    {
+                        restoringFile = reader.ReadLine();
+                        renamedFile = reader.ReadLine();
+                        File.Move(renamedFile, restoringFile);
+                        break;
+                    }
+
+                default: break;
+            }
+        }
+
+        public void RestoreTo(DateTime restorationTime)
         {
             if (fileWatcher.EnableRaisingEvents == true)
             {
                 return;
             }
 
-            StreamReader logger_reader = null;
-            DateTime current_logger_time;
-            string[] all_files = Directory.GetFiles(trackingDirectory, "*" + filter, SearchOption.AllDirectories);
-            foreach (var file in all_files)
+            StreamReader loggerReader = null;
+            DateTime currentLoggerTime;
+            string[] allFiles = Directory.GetFiles(trackingDirectory, "*" + filter, SearchOption.AllDirectories);
+            foreach (var file in allFiles)
             {
                 File.Delete(file);
             }
 
-            using (logger_reader = new StreamReader(loggerFullpath))
+            using (loggerReader = new StreamReader(loggerFullpath))
             {
-                bool result = DateTime.TryParse(logger_reader.ReadLine(), out current_logger_time);
+                bool result = DateTime.TryParse(loggerReader.ReadLine(), out currentLoggerTime);
                 if (!result)
                 {
                     throw new Exception("Parsing Error");
                 }
 
-                if (restoration_time < current_logger_time)
+                if (restorationTime < currentLoggerTime)
                 {
-                    restoration_time = current_logger_time;
+                    restorationTime = currentLoggerTime;
                 }
 
-                while (restoration_time >= current_logger_time)
+                while (restorationTime >= currentLoggerTime)
                 {
-                    string action = logger_reader.ReadLine();
-                    Recovery(logger_reader, action);
-                    if (logger_reader.Peek() < 0)
+                    string action = loggerReader.ReadLine();
+                    Recovery(loggerReader, action);
+                    if (loggerReader.Peek() < 0)
                     {
                         break;
                     }
 
-                    current_logger_time = DateTime.Parse(logger_reader.ReadLine());
+                    currentLoggerTime = DateTime.Parse(loggerReader.ReadLine());
                 }
 
-                Console.WriteLine("Restoring has been done");
-            }
-
-            void Recovery(StreamReader reader, string action)
-            {
-                string restoring_file;
-                string renamed_file;
-                string backup_file;
-                string deleted_file;
-                switch (action)
-                {
-                    case "Created":
-                        {
-                            restoring_file = reader.ReadLine();
-                            backup_file = reader.ReadLine();
-                            Directory.CreateDirectory(Path.GetDirectoryName(restoring_file));
-                            File.Copy(backup_file, restoring_file, true);
-                            break;
-                        }
-
-                    case "Changed":
-                        {
-                            restoring_file = reader.ReadLine();
-                            backup_file = reader.ReadLine();
-                            Directory.CreateDirectory(Path.GetDirectoryName(restoring_file));
-                            File.Copy(backup_file, restoring_file, true);
-                            break;
-                        }
-
-                    case "Deleted":
-                        {
-                            deleted_file = reader.ReadLine();
-                            File.Delete(reader.ReadLine());
-                            break;
-                        }
-
-                    case "Renamed":
-                        {
-                            restoring_file = reader.ReadLine();
-                            renamed_file = reader.ReadLine();
-                            File.Move(renamed_file, restoring_file);
-                            break;
-                        }
-
-                    default: break;
-                }
+                Console.WriteLine("Restoring is complete");
             }
         }
 
-        private void Save(string file_event, params string[] tracking_files_fullpath)
+        private void Save(string fileEvent, params string[] trackingFilesFullpath)
         {
             using (logger = new StreamWriter(loggerFullpath, true))
             {
                 var time = DateTime.Now.ToString();
-                foreach (var tracking_file_fullpath in tracking_files_fullpath)
+                foreach (var trackingFileFullpath in trackingFilesFullpath)
                 {
-                    string backup_filename = Path.Combine(backupDirectory, "BackUp" + filesCount.ToString() + ".txt");
+                    string backup_filename = Path.Combine(backupDirectory, "Backup" + filesCount.ToString() + ".txt");
                     string backup_file_fullpath = Path.Combine(backupDirectory, backup_filename);
-                    File.Copy(tracking_file_fullpath, backup_file_fullpath);
+                    File.Copy(trackingFileFullpath, backup_file_fullpath);
 
                     logger.WriteLine(time);
-                    logger.WriteLine(file_event);
-                    logger.WriteLine(tracking_file_fullpath);
+                    logger.WriteLine(fileEvent);
+                    logger.WriteLine(trackingFileFullpath);
                     logger.WriteLine(backup_file_fullpath);
 
                     filesCount++;
                 }
             }
-
-            logger.Close();
         }
 
-        private void FileSystemWatcher_OnCreated(object sender, FileSystemEventArgs e)
+        private void FileSystemWatcherOnCreated(object sender, FileSystemEventArgs e)
         {
             var fileInfo = new FileInfo(e.FullPath);
             if (!File.Exists(e.FullPath) || fileInfo.Extension != filter)
@@ -172,12 +169,12 @@ namespace Epam.Task06.BackUpSystem
                 return;
             }
 
-            Console.WriteLine($"File:{e.FullPath} has been created");
-            var trackingFileFullpath = e.FullPath;
-            Save("Created", trackingFileFullpath);
+            Console.WriteLine($"File:{e.FullPath} was created");
+            var tracking_file_fullpath = e.FullPath;
+            Save("Created", tracking_file_fullpath);
         }
 
-        private void FileSystemWatcher_OnRenamed(object sender, RenamedEventArgs e)
+        private void FileSystemWatcherOnRenamed(object sender, RenamedEventArgs e)
         {
             var fileInfo = new FileInfo(e.OldFullPath);
             if (!File.Exists(e.FullPath) || fileInfo.Extension != filter)
@@ -185,7 +182,7 @@ namespace Epam.Task06.BackUpSystem
                 return;
             }
 
-            Console.WriteLine($"File:{e.OldFullPath} has been renamed to {e.FullPath}");
+            Console.WriteLine($"File:{e.OldFullPath} was renamed to {e.FullPath}");
             var time = DateTime.Now.ToString();
             using (logger = new StreamWriter(loggerFullpath, true))
             {
@@ -194,11 +191,9 @@ namespace Epam.Task06.BackUpSystem
                 logger.WriteLine(e.FullPath);
                 logger.WriteLine(e.OldFullPath);
             }
-
-            logger.Close();
         }
 
-        private void FileSystemWatcher_OnChanged(object sender, FileSystemEventArgs e)
+        private void FileSystemWatcherOnChanged(object sender, FileSystemEventArgs e)
         {
             var fileInfo = new FileInfo(e.FullPath);
             if (!File.Exists(e.FullPath) || fileInfo.Extension != filter)
@@ -206,11 +201,11 @@ namespace Epam.Task06.BackUpSystem
                 return;
             }
 
-            Console.WriteLine($"File:{e.FullPath} has been changed");
+            Console.WriteLine($"File:{e.FullPath} was changed");
             Save("Changed", e.FullPath);
         }
 
-        private void FileSystemWatcher_OnDeleted(object sender, FileSystemEventArgs e)
+        private void FileSystemWatcherOnDeleted(object sender, FileSystemEventArgs e)
         {
             var fileInfo = new FileInfo(e.FullPath);
             if (!File.Exists(e.FullPath) || fileInfo.Extension != filter)
@@ -218,7 +213,7 @@ namespace Epam.Task06.BackUpSystem
                 return;
             }
 
-            Console.WriteLine($"File: {e.FullPath} deleted");
+            Console.WriteLine($"File: {e.FullPath} was deleted");
 
             using (logger = new StreamWriter(loggerFullpath, true))
             {
@@ -226,18 +221,11 @@ namespace Epam.Task06.BackUpSystem
                 logger.WriteLine("Deleted");
                 logger.WriteLine(e.FullPath);
             }
-
-            logger.Close();
         }
 
-        private void FileSystemWatcher_OnError(object source, ErrorEventArgs e)
+        private void FileSystemWatcherOnError(object source, ErrorEventArgs e)
         {
-            Console.WriteLine("The FileSystemWatcher has detected an error");
-
-            if (e.GetException().GetType() == typeof(InternalBufferOverflowException))
-            {
-                Console.WriteLine("The file system watcher experienced an internal buffer overflow: " + e.GetException().Message);
-            }
+            Console.WriteLine("The FileSystemWatcher was detected an error");
         }
     }
 }
--- Epam.Task05/Epam.Task05.BackupSystem/Form1.cs
+++ Epam.Task05/Epam.Task05.BackupSystem/Form1.cs
@@ -1,18 +1,20 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Windows.Forms;
-using Epam.Task06.BackUpSystem;
 
 namespace Epam.Task05.BackupSystem
 {
     public partial class Form1 : Form
     {
+        public string backup = @"C:\BackupSystem\Backup";
+        public string tracking = @"C:\BackupSystem\Tracking";
+        protected DateTime restoreTime;
+
         public Form1()
         {
             InitializeComponent();
 
-            string backup = @"C:\BackupSystem\Backup";
-            string tracking = @"C:\BackupSystem\Tracking";
-
+            button1.Visible = false;
             textBox1.Text = backup;
             textBox2.Text = tracking;
             textBox1.ReadOnly = true;
@@ -24,13 +26,30 @@ namespace Epam.Task05.BackupSystem
             {
                 Directory.CreateDirectory(tracking);
             }
-
-            BackUpSystem backUpSystem = new BackUpSystem(tracking, backup, ".txt", true);
         }
 
-        private void label2_Click(object sender, System.EventArgs e)
+        private void radioButton1CheckedChanged(object sender, EventArgs e)
         {
+            button1.Visible = false;
+            RadioButton radioButton = (RadioButton)sender;
+            if (radioButton.Checked)
+            {
+                MessageBox.Show("The tracking was started");
+            }
 
+            BackupSystem backupSystem = new BackupSystem(this.tracking, this.backup, ".txt", true);
+        }
+
+        private void radioButton2CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+        }
+
+        private void button1Click(object sender, EventArgs e)
+        {
+            this.restoreTime = dateTimePicker1.Value.Date + dateTimePicker2.Value.TimeOfDay;
+            BackupSystem backupSystem = new BackupSystem(this.tracking, this.backup, ".txt", false);
+            backupSystem.RestoreTo(this.restoreTime);
         }
     }
 }
